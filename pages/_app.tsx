b841eea5c511f89ef7bb396acba6957647d2a9dc
Eldar Kurmanaliev <ekurmanaliev@phoenixit.ru>
2021-05-21 22:31:09 +0300

    initial project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,76 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import AppLayout from '@shared/AppLayout';
+import '../src/styles/main.css';
+import LoginPage from './login';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const AppLoader = () => (
+  <div className='min-h-screen flex justify-center items-center'>
+    {/* <Loader /> */}
+    LOADER...
+  </div>
+);
+
+const AppContent = ({ Component, pageProps }: any) => {
+  const [content, setContent] = useState<any>(null);
+  const signedIn = false;
+  useEffect(() => {
+    let result;
+
+    const DefaultContent = () => (
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    );
+
+    result =
+      signedIn && Component.name !== 'LoginPage' ? (
+        <DefaultContent />
+      ) : (
+        <LoginPage />
+      );
+
+    setContent(result);
+  }, [signedIn, Component, pageProps]);
+
+  useEffect(() => {
+    return () => {
+      setContent(null);
+    };
+  }, []);
+
+  return content;
+};
+
+function App(props: any) {
+  //const title = useStore($title);
+  const [pageTitle, setPageTitle] = useState('Карта города');
+  const title = '';
+  useEffect(() => {
+    let resultTitle = 'Академия Лиги';
+    const additionalTitle = title ?? props.Component.config?.pageTitle;
+    if (additionalTitle) resultTitle += ` | ${additionalTitle}`;
+
+    setPageTitle(resultTitle);
+  }, [title]);
+
+  useEffect(() => {
+    return () => {
+      setPageTitle('Карта города');
+    };
+  }, []);
+  const authResponse = true;
+  const authError = true;
+  return (
+    <>
+      <Head>
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        <meta charSet='utf-8' />
+        <title>{pageTitle}</title>
+      </Head>
+      {!authResponse && !authError ? <AppLoader /> : <AppContent {...props} />}
+    </>
+  );
 }
-export default MyApp
+
+export default App;
---
+++ src/shared/AppLayout.tsx
@@ -0,0 +1,5 @@
+const AppLayout = ({ children }: any) => {
+  return <div className='bg-orange_3'>{children}</div>;
+};
+
+export default AppLayout;
